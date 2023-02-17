d40185deb8009a5e163b005861a00d93e8be763b
Agnieszka Trendowicz <agnieszka.trendowicz@gmail.com>
2023-02-18 00:25:52 +0100

    Issues filtration

--- src/main/java/com/bugtracker/issue/Issue.java
+++ src/main/java/com/bugtracker/issue/Issue.java
@@ -26,33 +26,38 @@ public class Issue {
     @Id
     @GeneratedValue
     private Long id;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Status status;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Priority priority;
+
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Type type;
+
     @Column(nullable = false, unique = true, length = 120)
     private String name;
     private String description;
     //    @Column(unique = true, length = 20)
-    //    private String code;
-    //    issue number? short name? relacja?
-    @ManyToOne
-    @JoinColumn(name = "project_id", nullable = false)
-    private Project project;
+    //    private String code; //issue number? short name? relacja?
     //    @ManyToOne
+    //    @JoinColumn(name = "project_id", nullable = false)
+    //    private Project project;
+    //    @ManyToOne     //trzeba wyciagnać info kto jest zalogowany
     //    @JoinColumn(name = "creator_id", nullable = false)
     //    private Person creator;
     @ManyToOne
     @JoinColumn(name = "assignee_id", nullable = false)
     private Person assignee;
+
     @Column(nullable = false)
     private final LocalDate dateCreated = LocalDate.now();
     private LocalDate lastUpdate;
+
     @OneToMany(mappedBy = "issue")
     private List<Comment> comments;
 
--- src/main/java/com/bugtracker/issue/IssueController.java
+++ src/main/java/com/bugtracker/issue/IssueController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import com.bugtracker.project.ProjectRepository;
+import com.bugtracker.enums.Priority;
+import com.bugtracker.enums.Status;
+import com.bugtracker.enums.Type;
 import com.bugtracker.person.PersonRepository;
+import com.bugtracker.project.ProjectRepository;
 
 @Controller
 @RequestMapping("/issues")
@@ -27,17 +30,27 @@ public class IssueController {
         this.projectRepository = projectRepository;
     }
 
-    @GetMapping("/list")
+    @GetMapping
 //    @Secured("ROLE_USERS_TAB")
-    public String users(Model model){
-        model.addAttribute("issues",issueRepository.findAll());
+    public String users(@ModelAttribute IssueFilter issueFilter, Model model) {
+        model.addAttribute("issues", issueRepository.findAll(issueFilter.buildQuery()));
+        model.addAttribute("assignedPerson", personRepository.findAll());
+        model.addAttribute("projects", projectRepository.findAll());
+        model.addAttribute("filter", issueFilter);
+        model.addAttribute("types", Type.values());
+        model.addAttribute("statuses", Status.values());
+        model.addAttribute("priorities", Priority.values());
+
         return "issue/issues";
     }
 
     @GetMapping("/create")
-    public String showIssueForm(Model model){
+    public String showIssueForm(Model model) {
         model.addAttribute("persons", personRepository.findAll());
         model.addAttribute("projects", projectRepository.findAll());
+        model.addAttribute("types", Type.values());
+        model.addAttribute("statuses", Status.values());
+        model.addAttribute("priorities", Priority.values());
         model.addAttribute("issue", new Issue());
         return "issue/add-issue";
     }
@@ -47,7 +60,7 @@ public class IssueController {
         System.out.println(issue);
         issueRepository.save(issue);
 
-        return new ModelAndView("redirect:/list");
+        return new ModelAndView("redirect:/issues");
     }
 //    @GetMapping("/issue")
 //    ModelAndView create() {
